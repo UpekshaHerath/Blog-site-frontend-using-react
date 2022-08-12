ce617635ab2eff7504f6a975c93f706f467f0826
UpekshaHerath <upekshadilshan000@gmail.com>
2022-08-12 15:53:22 +0530

    first commit

---
+++ src/components/Home.js
@@ -0,0 +1,34 @@
+import { useState, useEffect } from "react";
+import BlogList from "./BlogList";
+
+
+const Home = () => {
+    //This hook is use to two way data binding like process. We can provide a default value. And we can update and render later in the DOM
+    const [blogs, setBlogs] = useState(null);
+
+    const handleDelete = (id) => {
+        const newBlogs = blogs.filter(blog => blog.id !== id);
+        setBlogs(newBlogs);
+    }
+
+    // use effect hook executes after each rander of the application
+    // but when we pass a empty array as a parameter then it only renders when the first time this page renders
+    useEffect(() => {
+        fetch('http://localhost:8000/blogs')
+            .then(res => {
+                return res.json()
+            })
+            .then(data => {
+                console.log(data);
+                setBlogs(data);
+            })
+    }, []);
+
+    return (
+        <div className="home">
+            {blogs && <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />}
+        </div>
+    );
+}
+
+export default Home;
